810fa32d1fc08d0205befed50360f8c620d43678
elijahsamuels <elijahsamuels@gmail.com>
2020-11-13 12:43:20 -0700

    commit tree 969f34b7434e313f696daa0ec838a3042606e6b5

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,29 @@
-require 'pry'
+# collection = (1..100).to_a
+# my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 } 
+
+
+# require 'pry'
+
+# def my_find(collection)
+#     i = 0
+#     while i < collection.size
+#         if yield(collection[i])
+#             return collection[i]
+#         end
+
+#         i += 1
+#     end
+
+# end
+
+
+
 
 def my_find(collection)
+    i = 0
+    while i < collection.size
+            return collection[i] if yield(collection[i])        
+        i += 1
+    end
 
 end
